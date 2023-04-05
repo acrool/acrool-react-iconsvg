d638a6ca404c73df5cf735dc4a02c382110e52a0
imagine10255 <imagine10255@gmail.com>
2023-04-05 09:43:16 +0800

    refactor: sort vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,12 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
-        visualizer() as Plugin,
         eslint(),
+        react(),
         dts({
             insertTypesEntry: true,
         }),
+        visualizer() as Plugin,
     ],
     resolve: {
         alias: {
